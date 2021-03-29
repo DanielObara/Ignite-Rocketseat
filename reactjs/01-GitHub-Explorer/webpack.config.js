76913a6e9f02104ea16763c82ffdfa0ebc2445bf
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-03-29 11:30:11 -0300

    Fast Refresh no Webpack

--- reactjs/01-GitHub-Explorer/webpack.config.js
+++ reactjs/01-GitHub-Explorer/webpack.config.js
@@ -1,6 +1,7 @@
 
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -16,20 +17,27 @@ module.exports = {
       extensions: ['.js', '.jsx'],
   },
     devServer: {
-    contentBase: path.resolve(__dirname, "public")
+    contentBase: path.resolve(__dirname, "public"),
+    hot: true
   },
   plugins:[
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
-  ],
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
         }
       },
       {
