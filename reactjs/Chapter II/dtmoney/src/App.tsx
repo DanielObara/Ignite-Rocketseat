99ac7fc1f966d1a8d5b71120b9a7c84836c54b74
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-05-06 23:10:38 -0300

    Add styled components and GlobalStyle

--- reactjs/Chapter II/dtmoney/src/App.tsx
+++ reactjs/Chapter II/dtmoney/src/App.tsx
@@ -1,7 +1,9 @@
+import { GlobalStyle } from "./styles/global   "
 export function App() {
   return (
     <>
       <h1>Hello</h1>
+      <GlobalStyle />
     </>
   );
 }
---
+++ reactjs/Chapter II/dtmoney/src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+	:root {
+		--background: #f0f2f5;
+		--red: #e52e4d;
+		--blue: #5429cc;
+		--blue-light: #6933ff;
+		--text-title: #363f5f;
+		--text-body: #969cb3;
+		--background: #f0f2f5;
+		--shape: #ffff;
+	}
+	
+	*{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+	
+	/* font-size de 16px é bom para Desktop */
+	// REM = 1rem = font-size 
+	html{
+		@media (max-width: 1080px){
+			font-size: 93.75%; //15px
+		}
+		
+		@media(max-width: 720px){
+			font-size: 87.75%; //14px
+		}
+	}
+
+	body {
+	 background: var(--background);
+	 -webkit-font-smoothing: antialiased;	
+	}
+	
+	button{
+		cursor: pointer;
+	}
+	
+	[disabled] {
+		opacity: 0.6;
+		cursor: not-allowed;
+	}
+`;
