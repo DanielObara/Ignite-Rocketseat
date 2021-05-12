f4241b80e91c201828d09853078a4decd9fee910
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-05-11 22:39:06 -0300

    added modal library

--- reactjs/Chapter II/dtmoney/src/components/Header/index.tsx
+++ reactjs/Chapter II/dtmoney/src/components/Header/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
 export function Header() {
+	const [isClose, setIsClose] = useState(false)
 	return (
 		<Container>
 			<Content>
