35ca14e65cd0ba9a69b77c537db6f569d201909b
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-05-13 22:47:18 -0300

    Estrutura do form do modal

--- reactjs/Chapter II/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/Chapter II/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,25 @@
 import ReactModal from "react-modal";
+import { Container } from "./styles";
 
 interface newTransactionModalProps {
 	isOpen: boolean;
-	onRequestClose: ()=> void;
+	onRequestClose: () => void;
 }
 
-export function NewTransactionModal({ isOpen, onRequestClose }: newTransactionModalProps ){
+export function NewTransactionModal({ isOpen, onRequestClose }: newTransactionModalProps) {
 	return (
 		<ReactModal
 			isOpen={isOpen}
 			onRequestClose={onRequestClose}
+			overlayClassName="react-modal-overlay"
+			className="react-modal-content"
 		>
-			<h2>Cadastrar Transação</h2>
+			<Container>
+				<h2>Cadastrar Transação</h2>
+				<input type="text" placeholder="Titulo"/>
+				<input type="number" placeholder="Valor" />
+				<input type="text" placeholder="categoria" />
+			</Container>
 		</ReactModal>
 	)
 }
