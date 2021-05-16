aa4cee075f8fcb638882965c486f4c9b9d666c39
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-05-15 23:40:31 -0300

    Criando botões de Tipo

--- reactjs/Chapter II/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/Chapter II/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import ReactModal from "react-modal";
 import closeImg from '../../assets/close.svg'
-import { Container } from "./styles";
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from "./styles";
 
 interface newTransactionModalProps {
 	isOpen: boolean;
@@ -28,6 +30,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: newTransactionMo
 				<h2>Cadastrar Transação</h2>
 				<input type="text" placeholder="Titulo" />
 				<input type="number" placeholder="Valor" />
+				<TransactionTypeContainer>
+					<button
+						type="button"
+					>
+						<img src={incomeImg} alt="Entrada" />
+						<span>Entrada</span>
+					</button>
+					<button
+						type="button"
+					>
+						<img src={outcomeImg} alt="Saída" />
+						<span>Saída</span>
+					</button>
+				</TransactionTypeContainer>
+				
 				<input type="text" placeholder="categoria" />
 				<button type="submit">Cadastrar</button>
 			</Container>
