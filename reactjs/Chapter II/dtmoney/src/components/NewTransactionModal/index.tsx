ab4ee00785d7cb2cbaee30e2d5e381ec243af6d6
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-05-18 23:28:21 -0300

    button color

--- reactjs/Chapter II/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/Chapter II/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -38,6 +38,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: newTransactionMo
 						type="button"
 						onClick={() => {setType('deposit')}}
 						isActive={type === 'deposit'}
+						activeColor="green"
 					>
 						<img src={incomeImg} alt="Entrada" />
 						<span>Entrada</span>
@@ -46,6 +47,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: newTransactionMo
 						type="button"
 						onClick={() => { setType('withdraw') }}
 						isActive={type === 'withdraw'}
+						activeColor="red"
 					>
 						<img src={outcomeImg} alt="Saída" />
 						<span>Saída</span>
