268456ab41f30481752e42b11882d1638990b7a9
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-05-22 00:08:03 -0300

    Context API on react

--- reactjs/Chapter II/dtmoney/src/App.tsx
+++ reactjs/Chapter II/dtmoney/src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header"
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global"
+import { TransactionsContext } from "./TransactionsContext";
 
 
 ReactModal.setAppElement('#root');
@@ -20,13 +21,13 @@ export function App() {
   }
   
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal 
         isOpen={isNewTransactionModalOpen} 
         onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
