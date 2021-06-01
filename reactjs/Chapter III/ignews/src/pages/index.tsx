d167962aff3afa1735e32910afc5897e1f136f1f
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-05-31 21:29:26 -0300

    Dynamic title per page

--- reactjs/Chapter III/ignews/src/pages/index.tsx
+++ reactjs/Chapter III/ignews/src/pages/index.tsx
@@ -1,7 +1,12 @@
 import styles from '../../styles/home.module.scss'
-
+import Head from 'next/head'
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello</h1>
+    <>
+      <Head>
+        <title>Inicio</title>
+      </Head>
+      <h1 className={styles.title}>Hello</h1>
+    </>
   )
 }
