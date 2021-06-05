ff37a9ff5c82e995221880ef0435846674cd4c30
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-06-04 22:44:23 -0300

    Changed to Static Site Genetation

--- reactjs/Chapter III/ignews/src/pages/index.tsx
+++ reactjs/Chapter III/ignews/src/pages/index.tsx
@@ -1,5 +1,5 @@
 
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IyP6VJ6UCMH9UBh4DPy49L2')
 
   const product = {
@@ -50,6 +50,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24,
   }
 }
