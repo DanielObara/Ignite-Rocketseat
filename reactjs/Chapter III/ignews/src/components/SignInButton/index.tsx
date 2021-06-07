927d0c6865ccb4465b4ff3b8aaaf64288efb2e1c
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-06-07 08:30:33 -0300

    Initial next auth structure

--- reactjs/Chapter III/ignews/src/components/SignInButton/index.tsx
+++ reactjs/Chapter III/ignews/src/components/SignInButton/index.tsx
@@ -1,5 +1,7 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn } from 'next-auth/client'
+
 import styles from './styles.module.scss'
 
 export function SignInButton() {
@@ -11,7 +13,8 @@ export function SignInButton() {
 			<FiX color="#737380" className={styles.closeIcon} />
 		</button>
 	) : (
-			<button className={styles.signInButton}  type="button">
+		// TODO: add key to onclick
+			<button className={styles.signInButton} onClick={() => signIn('')} type="button">
 			<FaGithub color="#eba417" />
 				Sign in with GitHub
 
