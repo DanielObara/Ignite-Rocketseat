282e5b978241ae66040b951d2f282e3f4497b96d
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-06-08 23:42:22 -0300

    Added nextAuthProvider and useSession hook

--- reactjs/Chapter III/ignews/src/components/SignInButton/index.tsx
+++ reactjs/Chapter III/ignews/src/components/SignInButton/index.tsx
@@ -1,12 +1,14 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn } from 'next-auth/client'
+import { signIn, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-	const isUserLoggedIn = true;
-	return isUserLoggedIn ? (
+	const [session] = useSession();
+  console.log("🚀 ~ file: index.tsx ~ line 9 ~ SignInButton ~ session", session)
+	
+	return session ? (
 		<button className={styles.signInButton} type="button">
 			<FaGithub color="#04d361" />
 				Daniel Obara
@@ -14,7 +16,7 @@ export function SignInButton() {
 		</button>
 	) : (
 		// TODO: add key to onclick
-			<button className={styles.signInButton} onClick={() => signIn('')} type="button">
+			<button className={styles.signInButton} onClick={() => signIn('github')} type="button">
 			<FaGithub color="#eba417" />
 				Sign in with GitHub
 
