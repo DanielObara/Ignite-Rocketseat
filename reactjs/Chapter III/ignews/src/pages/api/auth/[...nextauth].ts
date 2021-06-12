a3ab3b3ff0540006823ce880c9744384bee93c6c
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-06-11 23:11:24 -0300

    remove jwt signinkey

--- reactjs/Chapter III/ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/Chapter III/ignews/src/pages/api/auth/[...nextauth].ts
@@ -14,9 +14,6 @@ export default NextAuth({
 		}),
 		// ...add more providers here
 	],
-	jwt:{
-		signingKey:process.env.SIGNING_KEY
-	},
 	callbacks: {
 		async signIn(user, account, profile) {
 			const {email} = user
