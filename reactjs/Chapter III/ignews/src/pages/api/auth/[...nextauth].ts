ff63da64a37658068cbeb45f99e7f7626ee88bae
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-06-11 23:10:11 -0300

    save user into DB

--- reactjs/Chapter III/ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/Chapter III/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
 	// Configure one or more authentication providers
@@ -11,7 +14,28 @@ export default NextAuth({
 		}),
 		// ...add more providers here
 	],
-
+	jwt:{
+		signingKey:process.env.SIGNING_KEY
+	},
+	callbacks: {
+		async signIn(user, account, profile) {
+			const {email} = user
+			
+			try {
+				await fauna.query(
+				q.Create(
+					q.Collection('users'),
+					{ data: { email } }
+				)
+			)
+ 
+			return true
+			} catch {
+				return false
+			}
+			
+		}
+	}
 	// A database is optional, but required to persist accounts in a database
-	database: process.env.DATABASE_URL,
+	// database: process.env.DATABASE_URL,
 })
