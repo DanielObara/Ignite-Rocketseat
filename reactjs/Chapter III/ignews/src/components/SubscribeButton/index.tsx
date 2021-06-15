1a1eb91ebcfb759bbb7c397a589a28db4ccd95a7
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-06-14 22:58:50 -0300

    Redirect to stripe do make payment

--- reactjs/Chapter III/ignews/src/components/SubscribeButton/index.tsx
+++ reactjs/Chapter III/ignews/src/components/SubscribeButton/index.tsx
@@ -1,20 +1,32 @@
 import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
 	priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 	const [session] = useSession()
-	
-	function handleSubscribe () {
-		if(!session){
+
+	async function handleSubscribe() {
+		if (!session) {
 			signIn('github')
 			return;
 		}
-		
-		
+
+		try {
+			const response = await api.post('/subscribe')
+
+			const { sessionId } = response.data
+
+			const stripe = await getStripeJs()
+
+			await stripe.redirectToCheckout({ sessionId })
+		} catch (error) {
+			alert(error.message);
+		}
 	}
 	return (
 		<button
