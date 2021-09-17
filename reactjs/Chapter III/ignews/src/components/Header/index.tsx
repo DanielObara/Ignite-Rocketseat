8b53ae7cf619b25c815550701dea8b9f3a2d70c1
Daniel Obara <daniel.tsutomu@hotmail.com>
2021-09-16 23:33:26 -0300

    feat: create active link component

--- reactjs/Chapter III/ignews/src/components/Header/index.tsx
+++ reactjs/Chapter III/ignews/src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
@@ -7,11 +9,15 @@ function Header() {
 			<div className={styles.headerContent}>
 				<img src="/images/logo.svg" alt="ignews" />
 				<nav>
-					<a className={styles.active}>Home</a>
-					<a href="">Posts</a>
+					<ActiveLink href="/" activeClassName={styles.active}>
+						<a className={styles.active}>Home</a>
+					</ActiveLink>
+					<ActiveLink href="/posts" activeClassName={styles.active}>
+						<a>Posts</a>
+					</ActiveLink>
 				</nav>
-				
-				<SignInButton/>
+
+				<SignInButton />
 			</div>
 		</header>
 	);
